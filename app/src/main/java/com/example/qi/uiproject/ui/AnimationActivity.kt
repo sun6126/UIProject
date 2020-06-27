d96dadea60682ef212d3cbfe2757ce0f83bac674
sunbaoqi <15951893223@13.com>
2020-06-27 08:55:08 +0800

    测试translationX、Y的作用

--- app/src/main/java/com/example/qi/uiproject/ui/AnimationActivity.kt
+++ app/src/main/java/com/example/qi/uiproject/ui/AnimationActivity.kt
@@ -3,11 +3,14 @@ package com.example.qi.uiproject.ui
 import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.animation.AnimationSet
 import com.example.qi.uiproject.R
 import kotlinx.android.synthetic.main.activity_animation.*
+import javax.security.auth.login.LoginException
 
 class AnimationActivity : AppCompatActivity() {
 
@@ -21,6 +24,12 @@ class AnimationActivity : AppCompatActivity() {
         btnAnimationSet.setOnClickListener { // 播放集合动画
             startSetAnimation()
         }
+        btnScroll.setOnClickListener {
+//            btnAnimationSet.translationX = 60f
+            btnAnimationSet.translationY = btnAnimationSet.translationY + 60f
+            Log.e("sunbaoqi",btnAnimationSet.x .toString()+ "     "+ btnAnimationSet.y) // translationX/Y 向右移是正，向下移是正
+//            btnAnimationSet.scrollBy(btnAnimationSet.x.toInt(),20) // scrollX和scrollY 移动内容，向上移是正，向下是负，向左移是正，向右移是负
+        }
     }
 
     // 集合动画
@@ -29,7 +38,7 @@ class AnimationActivity : AppCompatActivity() {
             tvTest,
             "translationX",
             -500f,
-            tvTest.translationX
+            500f
         )
 
         val alpha = ObjectAnimator.ofFloat(tvTest, "alpha", 0f, 1f)
---
+++ app/src/main/java/com/example/qi/uiproject/ui/TaoBaoHomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.qi.uiproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qi.uiproject.R
+
+class TaoBaoHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tao_bao_home)
+    }
+}
